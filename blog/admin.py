9976acdcaa4111cee7cb4c0c310f8ee3a82a0d35
Shahparan Rifat <shahranrifat115207@gmail.com>
2022-12-25 23:32:03 +0600

    changes in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Blog,Code, CustomUser
 # Register your models here.
+
+class User(admin.ModelAdmin):
+    list_display=('is_active')
 
 admin.site.register(Blog)
 admin.site.register(Code)
